cf71b6bcb3180c5d04a9cd167907d216aaea2ff5
Peyman M. <16763358+peymanr34@users.noreply.github.com>
2025-03-05 21:03:59 +0330

    Add public api tests.

---
+++ Source/Riviera.ZarinPal.Tests/PublicApiTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using PublicApiGenerator;
+using VerifyXunit;
+using Xunit;
+
+namespace Riviera.ZarinPal.Tests
+{
+    public class PublicApiTests
+    {
+        [Fact]
+        public Task PublicApiShouldNotChange()
+        {
+            var publicApi = typeof(V1.ZarinPalService).Assembly
+                .GeneratePublicApi();
+
+            return Verifier.Verify(publicApi)
+                .UniqueForTargetFrameworkAndVersion();
+        }
+    }
+}
